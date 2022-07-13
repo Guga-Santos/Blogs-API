8d63d8cb4bb9fd603bdac30df3c34ba706d6e6b3
GugaSantos <guga.santos.guga@gmail.com>
2022-07-13 15:45:33 -0300

    Feat: Criado o model 'User' em 'src/database/models/user.js' com as propriedades corretas

---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true 
+        },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Users',
+      underscored: true,
+    });
+  
+    return User;
+  };
