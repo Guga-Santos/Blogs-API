06be9aa7c9c52c8f274a051c306e6a2db2405481
GugaSantos <guga.santos.guga@gmail.com>
2022-07-14 02:47:20 -0300

    Feat: Criado Model 'PostCategory' com suas propriedades e associacoes corretas

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        postId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+            primaryKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+            primaryKey: true,
+        }
+    }, {
+        timestamps: false,
+    });
+
+    PostCategory.associate = (models) => {
+        models.Category.belongsToMany(models.BlogPost, {
+            through: PostCategory,
+            foreignKey: 'categoryId',
+            otherKey: 'postId',
+            as: 'posts',
+        });
+        models.BlogPost.belongsToMany(models.Category, {
+            through: PostCategory,
+            foreignKey: 'postId',
+            otherKey: 'categoryId',
+            as: 'posts',
+        });
+    }
+
+    return PostCategory;
+}
